67bfc02bff1d75ef9a6c48515c8203cb5ef056aa
IEEE Projects Bengaluru <105450161+IEEEProjectsBengaluru@users.noreply.github.com>
2022-09-24 23:49:55 +0530

    Add files via upload

---
+++ 1. Hello World shell script/HelloWorld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Display Hello world
+# To execute this file either use $ ./HelloWorld.sh or $ /vagrant/HelloWorld.sh
+
+echo "Hello World!"
+echo 'Good Morning'
---
+++ 1. Hello World shell script/myfile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# # = Sharp ! = Bang #! = Shebang and follwed by the path to interpreter
+# ./bin/bash <FILENAME> - which means the commands in this file will be executed by the interpreter
